5ad2515bc27b31c7467041ff332437b0cfb26e15
melody5417 <melody5417@melody5417deMacBook-Air.local>
2016-12-13 23:29:15 +0800

    增加地面检查 障碍物检查有bug

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -22,6 +22,15 @@ enum 物理层 {
     static let 地面: UInt32 =         0b100 // 4
 }
 
+enum 游戏状态 {
+    case 主菜单
+    case 教程
+    case 游戏
+    case 跌落
+    case 显示分数
+    case 结束
+}
+
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // k作为常量开头
@@ -38,6 +47,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var 速度 = CGPoint.zero
     var 撞击了地面 = false
     var 撞击了障碍物 = false
+    var 当前游戏状态: 游戏状态 = .游戏
     
     // 作为容纳整个游戏的容器
     let 游戏世界 = SKNode()
@@ -150,38 +160,53 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     // MARK: 游戏流程
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // 播放音效
-        run(拍打的音效)
         
-        // 增加上冲速度
-        主角飞一下()
+        switch 当前游戏状态 {
+            
+        case .主菜单:
+            break
+        case .教程:
+            break
+        case .游戏:
+            // 增加上冲速度
+            主角飞一下()
+            break
+        case .跌落:
+            break
+        case .显示分数:
+            break
+        case .结束:
+            break
+        }
+    }
+    
+    func 主角飞一下() {
+        速度 = CGPoint(x: 0, y: k上冲速度)
         
         // 移动帽子
         let 向上移动 = SKAction.moveBy(x: 0, y: 12, duration: 0.15)
         向上移动.timingMode = .easeInEaseOut
         let 向下移动 = 向上移动.reversed()
         帽子.run(SKAction.sequence([向上移动, 向下移动]))
-    }
-    
-    func 主角飞一下() {
-        速度 = CGPoint(x: 0, y: k上冲速度)
+        
+        // 播放音效
+        run(拍打的音效)
     }
     
     func 创建障碍物(图片名: String) -> SKSpriteNode {
         let 障碍物 = SKSpriteNode(imageNamed: 图片名)
         障碍物.zPosition = 图层.障碍物.rawValue
+        障碍物.userData = NSMutableDictionary()
         
         let offsetX = 障碍物.size.width * 障碍物.anchorPoint.x
         let offsetY = 障碍物.size.height * 障碍物.anchorPoint.y
         
         let path = CGMutablePath()
         
-        path.move(to: CGPoint(x: 27 - offsetX, y: 315 - offsetY))
-        path.addLine(to: CGPoint(x: 48 - offsetX, y: 310 - offsetY))
-        path.addLine(to: CGPoint(x: 51 - offsetX, y: 268 - offsetY))
-        path.addLine(to: CGPoint(x: 49 - offsetX, y: 0 - offsetY))
-        path.addLine(to: CGPoint(x: 3 - offsetX, y: 0 - offsetY))
-        path.addLine(to: CGPoint(x: 6 - offsetX, y: 310 - offsetY))
+        path.move(to: CGPoint(x: 4 - offsetX, y: 0 - offsetY))
+        path.addLine(to: CGPoint(x: 7 - offsetX, y: 307 - offsetY))
+        path.addLine(to: CGPoint(x: 47 - offsetX, y: 308 - offsetY))
+        path.addLine(to: CGPoint(x: 48 - offsetX, y: 1 - offsetY))
 
         path.closeSubpath()
         
@@ -203,11 +228,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         底部障碍.position = CGPoint(x: 起始X坐标, y: CGFloat.random(min: Y坐标最小值, max: Y坐标最大值))
         游戏世界.addChild(底部障碍)
+        底部障碍.name = "底部障碍"
         
         let 顶部障碍 = 创建障碍物(图片名: "CactusBottom")
         顶部障碍.zRotation = CGFloat(180).degreesToRadians()
         顶部障碍.position = CGPoint(x: 起始X坐标, y: 底部障碍.position.y + 底部障碍.size.height/2 + 底部障碍.size.height/2 + 主角.size.height * k缺口乘数)
         游戏世界.addChild(顶部障碍)
+        顶部障碍.name = "顶部障碍"
         
         let X轴移动距离 = -(size.width + 底部障碍.size.width)
         let 移动持续时间 = X轴移动距离 / k地面移动速度
@@ -228,7 +255,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let 无限重生 = SKAction.repeatForever(重生的动作队列)
         let 总的动作队列 = SKAction.sequence([首次延迟, 无限重生])
         
-        run(总的动作队列)
+        run(总的动作队列, withKey: "重生")
+    }
+    
+    func 停止重生障碍() {
+        removeAction(forKey: "重生")
+        
+        游戏世界.enumerateChildNodes(withName: "顶部障碍", using:
+            { 匹配单位, _ in
+                匹配单位.removeAllActions()
+        })
+        
+        游戏世界.enumerateChildNodes(withName: "底部障碍", using:
+            { 匹配单位, _ in
+                匹配单位.removeAllActions()
+        })
+
     }
     
     // MARK: 更新
@@ -242,8 +284,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         上一次更新时间 = 当前时间
         
-        更新主角()
-        更新前景()
+        switch 当前游戏状态 {
+        case .主菜单:
+            break
+        case .教程:
+            break
+        case .游戏:
+            更新前景()
+            更新主角()
+            撞击障碍物检查()
+            撞击地面检查()
+            break
+        case .跌落:
+            更新主角()
+            撞击地面检查()
+            break
+        case .显示分数:
+            break
+        case .结束:
+            break
+        }
     }
     
     func 更新主角() {
@@ -273,6 +333,45 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         })
     }
     
+    func 撞击障碍物检查() {
+        if 撞击了障碍物 {
+            撞击了障碍物 = false
+            切换到跌落状态()
+        }
+    }
+    
+    func 撞击地面检查() {
+        if 撞击了地面 {
+            撞击了地面 = false
+            速度 = CGPoint.zero
+            主角.zRotation = CGFloat(-90).degreesToRadians()
+            主角.position = CGPoint(x: 主角.position.x, y: 游戏区域起始点 + 主角.size.width/2)
+            run(撞击地面的音效)
+            切换到显示分数状态()
+        }
+    }
+    
+    func 切换到显示分数状态() {
+        当前游戏状态 = .显示分数
+        主角.removeAllActions()
+        停止重生障碍()
+    }
+    
+    // MARK: 游戏状态
+    
+    func 切换到跌落状态() {
+        
+        当前游戏状态 = .跌落
+        run(SKAction.sequence([
+            摔倒的音效,
+            SKAction.wait(forDuration: 0.1),
+            下落的音效
+            ]))
+        
+        主角.removeAllActions()
+        停止重生障碍()
+    }
+    
     // MARK: 物理引擎
     
     func didBegin(_ 碰撞双方: SKPhysicsContact) {
@@ -286,5 +385,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             撞击了障碍物 = true
         }
     }
+    
+    
     
 }
